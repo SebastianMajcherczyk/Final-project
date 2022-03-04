d422c7cda0a785dffa70488ec7c4cb05d3a4e5a1
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2022-03-04 08:29:57 +0100

    update gitignore

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,9 +2,25 @@ import React from "react";
 import ApiCalendar from 'react-google-calendar-api';
 
 const Calendar = () => {
+
+    const handleAddToCalendar = () => {
+        const eventFromNow = {
+            summary: 'Poc Dev From Now',
+            time: 480,
+        };
+
+        ApiCalendar.createEventFromNow(eventFromNow)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
-            ABC
+            <button onClick={handleAddToCalendar}>Add event</button>
         </div>
     )
 }
