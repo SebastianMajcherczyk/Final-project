30d8497cac94588c915f474a0bddc98b338acc25
SebastianMajcherczyk <seb.majcherczyk@gmail.com>
2022-03-26 10:14:54 +0100

    GH-pages fix - final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import React, {useEffect} from "react";
 import './App.scss';
-import  {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import  {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import {Header} from "./header/Header";
 import {Navi} from "./navi/Navi";
 import {ExchangeRates} from "./exchangerates/ExchangeRates";
 import {Main_Page} from "./mainpage/Main_Page";
 import {Mobilenavi} from "./hamburger/mobilenavi";
 
-
-
+const CustomRedirect = () => <Redirect path="/"/>
 
 function App() {
 
@@ -27,6 +26,7 @@ function App() {
                     <Switch>
                         <Route exact path="/"  component={Main_Page}/>
                         <Route exact path="/rates"  component={ExchangeRates}/>
+                        <Route exact path="*"  component={CustomRedirect}/>
                     </Switch>
 
                 </div>
